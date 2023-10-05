d3e4aef78f9c9df0e6e3b39fdae5560f5eaeafaf
Jakub Filko <jakubaakk@jakubfilko.cz>
2023-10-05 19:18:57 +0200

    Add logging

--- 00_system.sh
+++ 00_system.sh
@@ -1,57 +1,72 @@
 #!/bin/sh
 
-CUSTOM_HOSTNAME='grimmauld-place'
+LOG_FILE="log.txt"
+CUSTOM_HOSTNAME='burrow'
 
 #Configure hostname
+echo "Configuring hostname" >> "$LOG_FILE"
 sudo bash -c "echo $CUSTOM_HOSTNAME > /etc/hostname"
 
 #Configure hosts
+echo "Configuring hosts file" >> "$LOG_FILE"
 sudo bash -c "echo \"127.0.0.1       localhost\" >> /etc/hosts"
 sudo bash -c "echo \"::1             localhost\" >> /etc/hosts"
 sudo bash -c "echo \"127.0.1.1       $CUSTOM_HOSTNAME.localdomain $CUSTOM_HOSTNAME\" >> /etc/hosts"
 
 # Configure reflector
+echo "Configuring reflector" >> "$LOG_FILE"
 sudo pacman -Sy reflector
 sudo mkdir -p /etc/xdg/reflector && sudo cp reflector/reflector.conf /etc/xdg/reflector/reflector.conf
 sudo mkdir -p /etc/pacman.d/hooks && sudo cp reflector/mirrorupgrade.hook /etc/pacman.d/hooks/mirrorupgrade.hook
 sudo systemctl start reflector
 
 # Install Yay
+echo "Installing yay" >> "$LOG_FILE"
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. && rm -rf yay-bin
 yay --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --noremovemake --save
 
 # Install system apps
+echo "Installing xorg" >> "$LOG_FILE"
 yay -S xorg xorg-server
 
+echo "Installing system apps" >> "$LOG_FILE"
 APPS_SYSTEM=$(cat apps-system.txt | tr '\n' ' ')
 yay -S $APPS_SYSTEM
 
+echo "Installing Nvidia apps" >> "$LOG_FILE"
 APPS_NVIDIA=$(cat apps-nvidia.txt | tr '\n' ' ')
 yay -S $APPS_NVIDIA
 
+echo "Installing ASUS laptop apps" >> "$LOG_FILE"
 APPS_ASUS_LAPTOP=$(cat apps-asus-laptop.txt | tr '\n' ' ')
 yay -S $APPS_ASUS_LAPTOP
 
+echo "Installing dev apps" >> "$LOG_FILE"
 APPS_DEV=$(cat apps-dev.txt | tr '\n' ' ')
 yay -S $APPS_DEV
 
+echo "Installing generic apps" >> "$LOG_FILE"
 APPS_GENERIC=$(cat apps-generic.txt | tr '\n' ' ')
 yay -S $APPS_GENERIC
 
 
 # Configure Java
+echo "Configuring Java" >> "$LOG_FILE"
 sudo archlinux-java set zulu-11
 
 # Configure user groups
+echo "Configuring user groups" >> "$LOG_FILE"
 sudo usermod -aG docker $USER
 
 # Enable services
+echo "Enabling services" >> "$LOG_FILE"
 sudo systemctl enable NetworkManager
 sudo systemctl enable gdm
 sudo systemctl enable ufw
 sudo systemctl enable docker
 
 # Configure ufw
+echo "Configuring ufw" >> "$LOG_FILE"
 sudo systemctl start ufw
 sudo ufw default deny
 sudo ufw enable
--- 01_user.sh
+++ 01_user.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 # Install apps
+echo "Installing user apps" >> "$LOG_FILE"
 APPS=$(cat apps.txt | tr '\n' ' ')
 yay -S $APPS
 
 # Configure shell
+echo "Configuring shell" >> "$LOG_FILE"
 git clone https://github.com/jakubaakk/dotfiles.git
 cp dotfiles/* ~/.
 cp dotfiles/.* ~/.
 chsh -s /bin/zsh $USER
 
 # Install nvm and lts node
+echo "Installing nvm and lts node" >> "$LOG_FILE"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm i --lts
 
 # Git config
+echo "Configuring git config" >> "$LOG_FILE"
 git config --global user.name 'Jakub Filko'
 git config --global user.email 'jakubaakk@jakubfilko.cz'
